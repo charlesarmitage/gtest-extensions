e68d53a50cda5c780d39b08593f6882f254fc361
Charles Armitage <code@cjrarmitage.co.uk>
2013-04-20 08:39:18 +0100

    Add initial string comparison project using Catch

---
+++ StringCompCatch/TryCatch.cpp
@@ -0,0 +1,123 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include <algorithm>
+
+std::string create_msg(const std::string& match)
+{
+	std::stringstream message;
+	message << "Expected and actual differ at index: " << match.length() << ".\n";
+	message << "Matching section:\n";
+	message << "'" << match << "'\n";
+	return message.str();
+}
+
+std::string format_error_msg(const std::string& expected, const std::string& actual)
+{
+	if(expected == actual)
+	{
+		return "No error. Strings are identical.";
+	}
+	else if(expected.length() > actual.length())
+	{
+		return create_msg(actual);
+	}
+
+	auto result = std::mismatch(expected.begin(), expected.end(), actual.begin());
+	auto expected_position = result.first;
+	auto actual_position = result.second;
+	
+	std::string match;
+	if(expected_position != expected.end())
+	{
+		match = std::string(expected.begin(), expected_position);
+	}
+	else if(actual_position != actual.end())
+	{
+		match = expected;
+	}
+
+	return create_msg(match);
+}
+
+SCENARIO( "Display mis-matching strings" )
+{
+	GIVEN( "An expected string" )
+	{
+		std::string expected = "This is the expected string.";
+		
+		WHEN( "Actual differs at first character" )
+		{
+			auto actual = "A completely different string";
+
+			auto error_msg = format_error_msg(expected, actual);
+			REQUIRE( error_msg == "Expected and actual differ at index: 0.\n\
+Matching section:\n\
+\'\'\n");
+		}
+
+		WHEN( "Actual differs at ninth character" )
+		{
+			auto actual = "This is an unexpected string";
+
+			auto error_msg = format_error_msg(expected, actual);
+
+			REQUIRE( error_msg == "Expected and actual differ at index: 8.\n\
+Matching section:\n\
+\'This is \'\n");
+		}
+
+		WHEN( "Actual is shorter than expected string" )
+		{
+			auto actual = "This is the expected";
+
+			auto msg = format_error_msg(expected, actual);
+
+			REQUIRE( msg == "Expected and actual differ at index: 20.\n\
+Matching section:\n\
+\'This is the expected\'\n");
+		}
+
+		WHEN( "Actual is longer than expected string" )
+		{
+			auto actual = "This is the expected string. But this is not.";
+
+			auto error_msg = format_error_msg(expected, actual);
+
+			REQUIRE( error_msg == "Expected and actual differ at index: 28.\n\
+Matching section:\n\
+\'This is the expected string.\'\n");
+		}
+
+		WHEN( "Actual string is empty" )
+		{
+			auto actual = "";
+
+			auto error_msg = format_error_msg(expected, actual);
+
+			REQUIRE( error_msg == "Expected and actual differ at index: 0.\n\
+Matching section:\n\
+\''\n");
+		}
+
+		WHEN( "Expected string is empty" )
+		{
+			auto actual = "This is the expected string.";
+			auto expected = "";
+
+			auto error_msg = format_error_msg(expected, actual);
+
+			REQUIRE( error_msg == "Expected and actual differ at index: 0.\n\
+Matching section:\n\
+\''\n");
+		}
+
+		WHEN( "Strings are identical" )
+		{
+			auto actual = "This is the expected string.";
+
+			auto error_msg = format_error_msg(expected, actual);
+
+			REQUIRE( error_msg == "No error. Strings are identical." );
+		}
+	}
+}
