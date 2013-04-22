f39b51337c59e3c577dbea13aa05c6f30b6a993b
Charles Armitage <code@cjrarmitage.co.uk>
2013-04-22 19:08:15 +0100

    Move error formatting to seperate header file
    Move formatting to StringErrorFormat.h

---
+++ StringCompCatch/StringErrorFormat.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+std::string create_msg(const std::string& match)
+{
+	std::stringstream message;
+	message << "Expected and actual differ at index: " << match.length() << ".\n";
+	message << "Matching section:\n";
+	message << "'" << match << "'\n";
+	return message.str();
+}
+
+std::string format_error_msg(const std::string& expected, const std::string& actual)
+{
+	if(expected == actual)
+	{
+		return "No error. Strings are identical.";
+	}
+	else if(expected.length() > actual.length())
+	{
+		return create_msg(actual);
+	}
+
+	auto result = std::mismatch(expected.begin(), expected.end(), actual.begin());
+	auto expected_position = result.first;
+	auto actual_position = result.second;
+	
+	std::string match;
+	if(expected_position != expected.end())
+	{
+		match = std::string(expected.begin(), expected_position);
+	}
+	else if(actual_position != actual.end())
+	{
+		match = expected;
+	}
+
+	return create_msg(match);
+}
--- StringCompCatch/TryCatch.cpp
+++ StringCompCatch/TryCatch.cpp
@@ -1,43 +1,6 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-#include <algorithm>
-
-std::string create_msg(const std::string& match)
-{
-	std::stringstream message;
-	message << "Expected and actual differ at index: " << match.length() << ".\n";
-	message << "Matching section:\n";
-	message << "'" << match << "'\n";
-	return message.str();
-}
-
-std::string format_error_msg(const std::string& expected, const std::string& actual)
-{
-	if(expected == actual)
-	{
-		return "No error. Strings are identical.";
-	}
-	else if(expected.length() > actual.length())
-	{
-		return create_msg(actual);
-	}
-
-	auto result = std::mismatch(expected.begin(), expected.end(), actual.begin());
-	auto expected_position = result.first;
-	auto actual_position = result.second;
-	
-	std::string match;
-	if(expected_position != expected.end())
-	{
-		match = std::string(expected.begin(), expected_position);
-	}
-	else if(actual_position != actual.end())
-	{
-		match = expected;
-	}
-
-	return create_msg(match);
-}
+#include "StringErrorFormat.h"
 
 SCENARIO( "Display mis-matching strings" )
 {
