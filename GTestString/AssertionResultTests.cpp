559c3c8a3912324d9ab5ef6e4d5d2bb8ea8ff861
Charles Armitage <code@cjrarmitage.co.uk>
2013-04-23 22:22:18 +0100

    Add detailed string error reporting assert macro
    Use ASSERT_STRINGS_EQUAL macro to get detailed error reporting for
    gtest.

--- GTestString/AssertionResultTests.cpp
+++ GTestString/AssertionResultTests.cpp
@@ -1,31 +1,39 @@
 #include "gtest\gtest.h"
 
-::testing::AssertionResult AreStringsEqual(const std::string& lhs, const std::string& rhs)
+#include "StringErrorFormat.h"
+
+::testing::AssertionResult AreStringsEqual(const char* lhs_expected, const char* rhs_actual, const std::string& expected, const std::string& actual)
 {
-  if (lhs == rhs)
+  if (expected == actual)
     return ::testing::AssertionSuccess();
   else
     return ::testing::AssertionFailure(); //<< n << " is odd"; // TODO: This is where error messages can be streamed into the failure.
 }
 
-::testing::AssertionResult AreStringsEqualPred(const char* lhs_expr, const char* rhs_expr, const std::string& lhs, const std::string& rhs)
-{
-	return AreStringsEqual(lhs, rhs);
-}
+#define ASSERT_STRINGS_EQUAL(expected, actual) do{ ASSERT_PRED_FORMAT2(AreStringsEqual, expected, actual); }while(false)
 
 TEST(AssertionResultsTests, UsePredicateFormatAssert)
 {
-	ASSERT_PRED_FORMAT2(AreStringsEqualPred, "test string", "test string");
+	ASSERT_PRED_FORMAT2(AreStringsEqual, "test string", "test string");
+}
+
+TEST(AssertionResultsTests, UseCustomAssertMacro)
+{
+	ASSERT_STRINGS_EQUAL("test string", "test string");
 }
 
 TEST(AssertionResultTests, EqualStringsShouldReturnSuccessResult)
 {
-	auto result = AreStringsEqual("test string", "test string");
+	auto expected_str = "test string";
+	auto actual_str = "test string";
+	auto result = AreStringsEqual("expected_str", "actual_str", expected_str, actual_str);
 	ASSERT_EQ(::testing::AssertionSuccess(), result);
 }
 
 TEST(AssertionResultTests, DifferentStringsShouldReturnFailureResult)
 {
-	auto result = AreStringsEqual("test string", "a different string");
+	auto expected_str = "test string";
+	auto actual_str = "a different string";
+	auto result = AreStringsEqual("expected_str", "actual_str", expected_str, actual_str);
 	ASSERT_EQ(::testing::AssertionFailure(), result);
 }
