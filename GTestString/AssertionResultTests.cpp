e4761120d6ccd9392f23422b74fb127e0c0263be
Charles Armitage <code@cjrarmitage.co.uk>
2013-04-26 07:08:38 +0100

    Move string error asserts to separate header file

--- GTestString/AssertionResultTests.cpp
+++ GTestString/AssertionResultTests.cpp
@@ -1,23 +1,8 @@
+// Copyright C.Armitage 2013
+// Distributed under the FreeBSD license.
+// See accompanying file License.txt
 #include "gtest\gtest.h"
-
-#include "StringErrorFormat.h"
-
-::testing::AssertionResult AreStringsEqual(const char* expected_expr, const char* actual_expr, const std::string& expected, const std::string& actual)
-{
-  if (expected == actual)
-    return ::testing::AssertionSuccess();
-  
-  auto failure_msg = ::testing::AssertionFailure();
-
-  failure_msg << "'" << expected_expr << "' & " << "'" << actual_expr << "' are not equal.\n";
-  failure_msg << "Expected: " << expected << '\n';
-  failure_msg << "Actual:   " << actual << '\n';
-  failure_msg << format_error_msg(expected, actual);
-
-  return failure_msg;
-}
-
-#define ASSERT_STRINGS_EQUAL(expected, actual) do{ ASSERT_PRED_FORMAT2(AreStringsEqual, expected, actual); }while(false)
+#include "DetailedStringAsserts.h"
 
 TEST(AssertionResultsTests, UsePredicateFormatAssert)
 {
